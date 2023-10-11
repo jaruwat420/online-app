3be5e6dc3ae6349f5b803d909d479464ef70f3f7
jaruwat420 <jaruwatcj@gmail.com>
2023-10-11 11:35:01 +0700

    add partials

--- index.js
+++ index.js
@@ -5,15 +5,21 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const app = express();
 
 // Config Port
 const port = 3000;
 
+
 // Handlebars
-app.engine('handlebars', engine(
-    {defaultLayout: 'main', 
-    layoutsDir: path.join(__dirname, 'views/layouts')
+app.use(express.static(__dirname + '/public'));
+
+app.engine('handlebars', engine({
+    defaultLayout: 'main', 
+    layoutsDir: path.join(__dirname, 'views/layouts'),
+    partialsDir: path.join(__dirname, 'views/partials')
+
 }));
 
 app.set('view engine', 'handlebars');
@@ -33,7 +39,6 @@ app.get('/about', (req, res) =>{
         description: "lorem1000000"
     });
 })
-
 
 
 app.listen(port, ()=>{
